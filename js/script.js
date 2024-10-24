23de11c5d687104a446be8b2a502a508bcfc4c89
Anready <104269567+Anready@users.noreply.github.com>
2024-10-24 18:00:46 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,35 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const scriptList = document.getElementById('script-list');
 
-    // load file JSON
     fetch('scripts.json')
         .then(response => response.json())
         .then(data => {
-            const scripts = data.scripts;
-
-            scripts.sort((a, b) => a.name.localeCompare(b.name));
-
-            // add scripts to the list
-            scripts.forEach(script => {
+            Object.entries(data).forEach(([key, value]) => {
                 const li = document.createElement('li');
-                li.innerHTML = `${script.name} <button class="download-btn" data-url="${script.url}">Download</button>`;
+                li.textContent = `${key}: ${value}`;
                 scriptList.querySelector('ul').appendChild(li);
             });
-
-            // add click event to download button
-            const downloadButtons = document.querySelectorAll('.download-btn');
-            downloadButtons.forEach(button => {
-                button.addEventListener('click', () => {
-                    const scriptUrl = button.getAttribute('data-url');
-                    alert(`Downloading ${scriptUrl}...`);
-                    const link = document.createElement('a');
-                    link.href = scriptUrl;
-                    link.download = scriptUrl.split('/').pop(); // Extracts the file name from the URL
-                    document.body.appendChild(link);
-                    link.click();
-                    document.body.removeChild(link);
-                });
-            });
         })
-        .catch(error => console.error('Error loading JSON file:', error));
+        .catch(error => console.error('Error:', error));
 });
